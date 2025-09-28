42fa9c6244b5296360074638029fc50a24cf3e68
Abdulhakeem Sonayon Sulaimon <akboysulekz@gmail.com>
2025-09-28 20:38:08 +0100

    Update ExamInterface.tsx

--- src/components/ExamInterface.tsx
+++ src/components/ExamInterface.tsx
@@ -7,10 +7,6 @@ import { Alert, AlertDescription } from "@/components/ui/alert";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { MathRenderer } from "@/components/ui/math-renderer";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
-
-<MathJaxContext>
-  <MathJax>{question.text}</MathJax>
-</MathJaxContext>
 import { 
   Clock, 
   ChevronLeft, 
