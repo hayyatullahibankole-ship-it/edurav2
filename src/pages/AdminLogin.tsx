82b8ee29af253bc731037f2b54d71e69283de029
akboy825-gif <akboy825@gmail.com>
2026-04-01 07:18:19 +0100

    Update AdminLogin.tsx

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -49,7 +49,7 @@ export default function AdminLogin() {
   }
 
   if (user && isAdmin) {
-    return <Navigate to="/admin" replace />;
+    return <Navigate to="/#/admin" replace />;
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
